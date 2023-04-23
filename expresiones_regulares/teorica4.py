73aee0ab3f5fc813275d4fa87dada9a597d02243
Lorenzo Nastri <lorenzonastri@gmail.com>
2023-04-23 19:28:49 -0300

    notas

--- expresiones_regulares/teorica4.py
+++ expresiones_regulares/teorica4.py
@@ -76,20 +76,25 @@ print(re.sub(patron, "###", texto)) # Reemplaza ipsum por ###
 
     - Examina a través de la string («cadena») buscando el primer lugar donde el pattern («patrón») de la expresión regular produce una coincidencia, y retorna un objeto match correspondiente. Retorna None si ninguna posición en la cadena coincide con el patrón; notar que esto es diferente a encontrar una coincidencia de longitud cero en algún punto de la cadena.
     - De por sí no devuelve nada. Devuelve 'None' y por eso usamos bool.
+    - Busca en todo el texto la primera aparición.
 
 2) re.match(pattern, string, flags=0) 
 
     - Si cero o más caracteres al principio de la string («cadena») coinciden con el pattern («patrón») de la expresión regular, retorna un objeto match correspondiente. Retorna None si la cadena no coincide con el patrón; notar que esto es diferente de una coincidencia de longitud cero.
+    - De por sí no devuelve nada. Devuelve 'None' y por eso usamos bool.
 
 3) re.split(pattern, string, maxsplit=0, flags=0)
 
     - Divide la string («cadena») por el número de ocurrencias del pattern («patrón»). Si se utilizan paréntesis de captura en pattern, entonces el texto de todos los grupos en el patrón también se retornan como parte de la lista resultante. Si maxsplit (máxima divisibilidad) es distinta de cero, como mucho se producen maxsplit divisiones, y el resto de la cadena se retorna como elemento final de la lista.
+
 
 4) re.findall(pattern, string, flags=0)
 
     - Retorna todas las coincidencias no superpuestas de pattern en string, como una lista de strings o tuplas. El string se escanea de izquierda a derecha y las coincidencias se retornan en el orden en que se encuentran. Las coincidencias vacías se incluyen en el resultado.
     
     - El resultado depende del número de grupos detectados en el patrón. Si no hay grupos, retorna una lista de strings que coincidan con el patrón completo. Si existe exactamente un grupo, retorna una lista de strings que coincidan con ese grupo. Si hay varios grupos presentes, retorna una lista de tuplas de strings que coinciden con los grupos. Los grupos que no son detectados no afectan la forma del resultado.
+
+    - Te va a traer todas las apariciones.
 
 
 # Para hacer un patrón que queremos que lea más de una letra --> hay que usar *^, etc porque sino los matches van a ser de solamente un caracter. 
