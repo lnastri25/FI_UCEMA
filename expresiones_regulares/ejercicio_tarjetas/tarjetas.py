7521ac571f2a8d00454d0b96d466b611a5c4b25d
Lorenzo Nastri <lorenzonastri@gmail.com>
2023-04-24 08:45:11 -0300

    ejercicios tarjetas

---
+++ expresiones_regulares/ejercicio_tarjetas/tarjetas.py
@@ -0,0 +1,4 @@
+#!/bin/python3
+
+# Buscar el número de la tarjeta de crédito de las personas que son hombres y extraer 3 dígitos que estén en secuencia.
+# Buscar el número de la tarjeta de crédito de las personas que son mujeres y extraer 3 dígitos que NO estén en secuencia.
