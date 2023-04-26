8e3257c132e39c4a7062d27b0673e14087fd7fc4
Lorenzo Nastri <lorenzonastri@gmail.com>
2023-04-26 14:05:52 -0300

    modificaciones

--- simulacro_1_er_parcial-lnastri25_1/ej1.py
+++ simulacro_1_er_parcial-lnastri25_1/ej1.py
@@ -10,21 +10,32 @@ def subsecuencias(string):
 
 print(subsecuencias("XbaaaYjXababYqbabbbaaYqXffeeY"))
 
-# "X([^XY]*ab[^XY]*)Y" --> Otra manera de hacerlo.
+""""
+"X([^XY]*ab[^XY]*)Y" --> Otra manera de hacerlo.
+"""
 
 # Onomatopopih esta aprendiendo expresiones regulares y le pidieron construir una función que sea capaz de extraer la lista de substrings delimitadas por patrones 'ag' y 'cta' y no incluyan números. Revisa su código propuesto y marca con una x las opciones correctas. JUSTIFICA tus respuestas
 
-def funcionDeExpresiones_Regulares(string):
-    return re.findall('ag(\D.*?)cta',string)
+# def funcionDeExpresiones_Regulares():
+#   return re.findal('ag(\d.*?)cta')
 
-print(funcionDeExpresiones_Regulares('aabocaggaaactazu4lggaasag24gra1ndecta'))
+# print(funcionDeExpresiones_Regulares('aabocaggaaactazu4lggaasag24gra1ndecta'))
 
-# A) El nombre de la función de Onomatopopih respeta las convenciones de nombres de Python. --> FALSO. No hay error de código, solamente que en Python se suele usar snake_case y acá se puede observar que hay una mezcla entre camelCase y snake_case.
+# A) El nombre de la función de Onomatopopih respeta las convenciones de nombres de Python. --> FALSO. No hay error de código, solamente que en Python se suele usar snake_case y acá se puede observar que hay una mezcla entre camelCase y snake_case. Además, hace falta pasarle un parámetro a la misma y no tiene un nombre expresivo la misma. De hecho, ya sabemos que es una función porque retorna algo --> entonces no hay que ponerle "funcionede..." --> es super amplio.
 
-# B) La función lanza NameError al ser ejecutada. --> FALSO. Un NameError ocurre cuando tratas de usar una variable o una funcion que no fue definida previamente.
+# B) La función lanza NameError al ser ejecutada. --> FALSO. Un NameError ocurre cuando tratas de usar una variable o una funcion que no fue definida previamente. El error que ocurre es AtributeError porque findall está mal escrito (re.findal)
 
-# C) La función lanza SyntaxError al ser ejecutada. --> FALSO. Un SyntaxError curre cuando el intérprete se encuentra con un error de sintaxis en el codigo.
+# C) La función lanza SyntaxError al ser ejecutada. --> FALSO. Un SyntaxError curre cuando el intérprete se encuentra con un error de sintaxis en el codigo. El error que ocurre es AtributeError porque findall está mal escrito (re.findal)
 
-# D) Una vez corregida la función, cuando se la invoca usando el texto 'aabocaaggaaactazu4lgaaasad24gra1ndecta' devuelve ['gaaa'] --> VERDADERO. Cumple con lo que se pide en la consigna. La modificación '\D' hace referencia a la no incluision de caracteres numericos.
+def delimitacion_de_patrones(string):
+    return re.findall("ag(\D*?)cta",string)
+
+print(delimitacion_de_patrones('aabocaggaaactazu4lggaasag24gra1ndecta'))
+
+""""
+"ag([^\d]*)cta" --> Otra manera de hacerlo.
+"""
+
+# D) Una vez corregida la función, cuando se la invoca usando el texto 'aabocaaggaaactazu4lgaaasad24gra1ndecta' devuelve ['gaaa'] --> VERDADERO. Cumple con lo que se pide en la consigna. La modificación '\ag(\D*?)cta' hace referencia a la no incluision de caracteres numericos.
 
 # E) Una vez corregida la función, cuando se la invoca usando el texto 'aabocaaggaaaactazu4lgaaasad24gra1ndecta' devuelve ['23gra1nde'] --> FALSO. No cumplió lo que se pidió en el enunciado. Retorna lo que está entre 'ag' y 'cta' sin incluirlos, pero devuelve números también y en el enunciado pide que no se incluyan esos números.
