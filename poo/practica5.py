aabd4c1a4191f0e055b4e31fa566e97161c66b40
Lorenzo Nastri <lorenzonastri@gmail.com>
2023-05-01 11:55:13 -0300

    nuevo commit

--- poo/practica5.py
+++ poo/practica5.py
@@ -184,7 +184,7 @@ calculadora.valorActual()
 
 # Ejercicio 7 #
 # Definí una clase de gorriones, de los cuales nos interesa conocer dos medidas conocidas como CSS (coeficiente de serenidad silenciosa), CSSP y CSSV (como el CSS pero “pico” y “veces”). El CSS resulta de dividir la cantidad total de kilómetros que vuela desde que se lo comienza a estudiar, por la cantidad total de gramos de comida que ingiere. El CSSP es la misma división pero considerando solamente lo que voló la vez que más voló y lo que comió la vez que más comió. El CSSV es otra vez la misma idea, respecto de la cantidad de veces que voló y comió. Si un gorrión nunca comió, los coeficientes deben ser None. Un gorrión se considera en equilibrio si su CSS está entre 0.5 y 2.
-    
+  
 "https://github.com/AJVelezRueda/Fundamentos_de_informatica/blob/master/POO/Practica/Pr%C3%A1ctica_Objetos_Parte_2.md" # --> GitHub
 
 ## GUÍA 5: PRACTICA POO PARTE 2 ##
