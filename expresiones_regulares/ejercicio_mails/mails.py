ddd2da9b75e966d7603c715e5dfdacb81e204458
Lorenzo Nastri <lorenzonastri@gmail.com>
2023-05-07 09:19:13 -0300

    notas

--- expresiones_regulares/ejercicio_mails/mails.py
+++ expresiones_regulares/ejercicio_mails/mails.py
@@ -29,3 +29,20 @@ with open(archivo_destino, "w") as archivo_destino:
     archivo_destino.write(nuevo_contenido)
 
 print("Se ha creado el archivo con los correos electrónicos reemplazados exitosamente.")
+
+"""
+email --> "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
+
+--> [a-zA-Z0-9._%+-]+ 
+    coincide con una o más letras, números o caracteres especiales que se permiten en una dirección de correo electrónico, como puntos (.), guiones (-), porcentajes (%) o signos más (+).
+--> @ 
+    coincide con el carácter de arroba que separa el nombre de usuario del dominio en una dirección de correo electrónico.
+--> [a-zA-Z0-9.-]+ 
+    coincide con una o más letras, números, guiones o puntos en el dominio.
+--> \. 
+    coincide con un punto literal en la dirección de correo electrónico.
+--> [a-zA-Z]{2,} 
+    coincide con dos o más letras en la parte del dominio de nivel superior (TLD) de la dirección de correo electrónico. Por ejemplo, .com, .org, .edu.
+
+gmail --> "[a-zA-Z0-9._%+-]+@gmail\.com"
+"""
--- introduccion_python/teorica1.py
+++ introduccion_python/teorica1.py
@@ -46,4 +46,34 @@ if __name__ == "__main__":
   - cd (change directory)
   - pwd (path to working directory; donde estoy trabajando; muestra el directorio actual)
   - ls -a (lista los archivos ocultos tambien)
+
+8) ADICIONALES:
+  --> *Funciones*:
+    Una función es un bloque de código que realiza una tarea específica y devuelve un valor. Las funciones pueden tener parámetros de entrada y un valor de retorno. Las funciones son autónomas y se pueden llamar desde cualquier parte del programa.
+
+--> *Procedimientos*:
+    Un procedimiento es similar a una función en que es un bloque de código que realiza una tarea específica. Sin embargo, a diferencia de las funciones, los procedimientos no tienen un valor de retorno. Los procedimientos pueden tener parámetros de entrada, pero su propósito principal es realizar una acción específica en lugar de devolver un valor.
+
+--> *Diferencias entre snake_case y Camel_Case:*
+
+    La razón técnica detrás de la utilización de convenciones de nomenclatura como camel case o snake case es que, al nombrar variables, funciones y otros elementos de código de manera consistente, se facilita la lectura y comprensión del código para otros programadores. Esto puede ser especialmente útil en proyectos grandes o en equipo, donde muchas personas trabajan en el mismo código.
+    Además, seguir una convención de nomenclatura consistente puede ayudar a evitar errores y reducir el tiempo que se tarda en entender y modificar el código. Al utilizar una convención de nomenclatura clara y coherente, se hace más fácil identificar rápidamente qué hacen las variables y funciones, y cómo están relacionadas entre sí. En última instancia, esto puede ayudar a mejorar la calidad y eficiencia del código.
+
+
+    --> *Convención de Nomenclatura Consistente*:
+    La convención de nomenclatura consistente se refiere al conjunto de reglas y prácticas que se siguen para nombrar variables, funciones y otros elementos de código en un proyecto de programación. La idea es establecer una forma coherente y uniforme de nombrar los elementos del código para que sea más fácil de leer, entender y mantener por los desarrolladores que trabajan en el proyecto.
+
+    Por ejemplo, una convención de nomenclatura consistente podría ser nombrar las variables en minúsculas con guiones bajos para separar las palabras, como "nombre_de_variable". Otra convención podría ser nombrar las funciones con verbos que reflejen su acción, como "calcular_suma". Siguiendo una convención de nomenclatura consistente, se puede mejorar la legibilidad y la calidad del código, y hacer que el proceso de programación sea más eficiente y efectivo.
+
+---------------------------
+
+--> *Estructura de codigo limpia:
+
+    Legibilidad: Un código limpio es más fácil de leer y entender, no solo para uno mismo, sino también para otros programadores que puedan trabajar en el proyecto en el futuro. Si el código es difícil de entender, puede resultar en errores y problemas de mantenimiento.
+
+    Mantenibilidad: Un código limpio y bien estructurado es más fácil de modificar y mantener a lo largo del tiempo. Si el código no está estructurado correctamente, puede ser difícil identificar dónde se encuentra un problema o cómo se relacionan diferentes partes del código.
+
+    Eficiencia: Un código limpio y ordenado puede ser más eficiente en términos de rendimiento, ya que puede requerir menos recursos y tiempo para ejecutarse.
+
+    Colaboración: Cuando se trabaja en un proyecto de equipo, seguir una estructura de código limpia y ordenada puede facilitar la colaboración y el trabajo en equipo. Todo el mundo puede entender rápidamente lo que está sucediendo en el proyecto y contribuir de manera efectiva.
 """
