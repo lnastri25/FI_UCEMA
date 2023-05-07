0c0b9c42a02b8dc2c8f5c57bc4fe7e59b92ac0db
Lorenzo Nastri <lorenzonastri@gmail.com>
2023-05-07 20:37:04 -0300

    notas

--- introduccion_python/teorica1.py
+++ introduccion_python/teorica1.py
@@ -48,7 +48,8 @@ if __name__ == "__main__":
   - ls -a (lista los archivos ocultos tambien)
 
 8) ADICIONALES:
-  --> *Funciones*:
+
+--> *Funciones*:
     Una función es un bloque de código que realiza una tarea específica y devuelve un valor. Las funciones pueden tener parámetros de entrada y un valor de retorno. Las funciones son autónomas y se pueden llamar desde cualquier parte del programa.
 
 --> *Procedimientos*:
@@ -57,6 +58,7 @@ if __name__ == "__main__":
 --> *Diferencias entre snake_case y Camel_Case:*
 
     La razón técnica detrás de la utilización de convenciones de nomenclatura como camel case o snake case es que, al nombrar variables, funciones y otros elementos de código de manera consistente, se facilita la lectura y comprensión del código para otros programadores. Esto puede ser especialmente útil en proyectos grandes o en equipo, donde muchas personas trabajan en el mismo código.
+
     Además, seguir una convención de nomenclatura consistente puede ayudar a evitar errores y reducir el tiempo que se tarda en entender y modificar el código. Al utilizar una convención de nomenclatura clara y coherente, se hace más fácil identificar rápidamente qué hacen las variables y funciones, y cómo están relacionadas entre sí. En última instancia, esto puede ayudar a mejorar la calidad y eficiencia del código.
 
 ---------------------------
