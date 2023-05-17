62f6ff06d7d520a0c33a7e3be3f5ebc304b28ab3
Lorenzo Nastri <lorenzonastri@gmail.com>
2023-05-17 14:04:37 -0300

    manejo de excepciones

--- manejo_de_excepciones/practica7.py
+++ manejo_de_excepciones/practica7.py
@@ -10,13 +10,21 @@ try:
     lista_super + "arroz"
 except TypeError: # le agregué el error
     print("No puedo agregar arroz")
-    
+
 # ¿Es correcto el uso del try...except? ¿Qué cosa/s le modificarías?
 
 # Desde mi punto de vista, NO es correcto el uso del try...except, ya que no se está especificando el tipo de error que se quiere manejar. Se debería especificar el tipo de error que se quiere manejar, por ejemplo TypeError.
 
 # Ejercicio 2 #
 # Definir una función que tenga como parámetros una lista de números por un lado y un número por otro y devuelva una lista con la división de cada elemento por el número dado. Por ejemplo, si le paso ([2,4,6,8], 2), debería retornar [1,2,3,4]. Tomar las precauciones necesarias.
+
+def division(lista, numero):
+    try:
+        return [elemento/numero for elemento in lista]
+    except ZeroDivisionError:
+        print("No se puede dividir por", numero)
+
+print(division([2,4,6,8], 2))
 
 # Ejercicio 3 #
 # Definir un procedimiento, que reciba una lista y un número, el cual tiene que agregar el número a la lista solo si el número es positivo. De lo contrario debería generar un error indicando que el número no es positivo.
