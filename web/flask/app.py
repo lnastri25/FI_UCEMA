8bf9c107172b2d7192f3157f98164e7dba8b54e5
Lorenzo Nastri <lorenzonastri@gmail.com>
2023-05-22 13:00:59 -0300

    flask

--- web/flask/app.py
+++ web/flask/app.py
@@ -1,7 +1,14 @@
 from flask import Flask
 
+prendas = [
+    {"id": 1, "tipo": "pantalon", "talle": 42},
+    {"id": 2, "tipo": "pantalon", "talle": 56}
+]
+
 app = Flask(__name__) # Me refiero a la aplicación por la palabra app. App va a ser el nombre que le voy a estar dando a mi servidor.
 
 @app.get("/") # Decorador. Le estoy diciendo a mi servidor que cuando alguien entre a la ruta '/' (la ruta principal), ejecute la función que viene a continuación.
 def home():
     return "<p>Te damos la bienvenida a MacoWins</p>"
+
+# TAREA: Armar la ruta /prendas que muestre todos los items de prendas. OPCIONAL: ESCRIBIR O AGREGAR UN ELEMENTO.
