d336d6c69c904d257cbea4806fadd02bcc787e0f
Lorenzo Nastri <lorenzonastri@gmail.com>
2023-06-21 17:27:47 -0300

    web y flask

--- web/practica_flask2/app.py
+++ web/practica_flask2/app.py
@@ -1,27 +1,25 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from markupsafe import escape
-
-# python3 -m flask run #
 
 app = Flask(__name__)
 
 productos = {
-    1: {"nombre": "Shampoo sólido", "Stock": 5, "precio": 1000},
-    2: {"nombre": "Crema de manos", "Stock": 6, "precio": 2000},
-}
+1: {"name": "Shampoo sólido", "Stock": 5, "Precio_unitario": 300},
+2: {"name": "Crema de manos", "Stock": 6, "Precio_unitario": 600}}
+
 
 @app.get("/")
 def home():
-    return render_template("home.html") 
+    return render_template("home.html")
 
 @app.get("/productos/")
-def get_all_productos():
+def get_all_products():
     return render_template("productos.html", productos=productos.items())
 
-@app.get("/productos/<int:id>/")
-def get_producto(aidi):
-    if aidi in productos:
-        producto = productos[aidi]
-        return render_template('producto.html', id=aidi, producto=producto)
+@app.get("/productos/<int:id>")
+def get_producto(id):
+    if id in productos:
+        producto = productos[id]
+        return render_template('producto.html', id=id, producto=producto)
     else:
-        return ("No hay producto", 404)
+        return ("no hay producto", 404)
