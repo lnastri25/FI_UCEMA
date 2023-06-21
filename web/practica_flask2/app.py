b65751c0d88933e1819414df8210fc77cdc0a73e
Lorenzo Nastri <lorenzonastri@gmail.com>
2023-06-21 14:48:47 -0300

    web, http y flask

--- web/practica_flask2/app.py
+++ web/practica_flask2/app.py
@@ -5,6 +5,23 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+productos = {
+    1: {"nombre": "Shampoo sólido", "Stock": 5, "precio": 1000},
+    2: {"nombre": "Crema de manos", "Stock": 6, "precio": 2000},
+}
+
 @app.get("/")
 def home():
     return render_template("home.html") 
+
+@app.get("/productos/")
+def get_all_productos():
+    return render_template("productos.html", productos=productos.items())
+
+@app.get("/productos/<int:id>/")
+def get_producto(aidi):
+    if aidi in productos:
+        producto = productos[aidi]
+        return render_template('producto.html', id=aidi, producto=producto)
+    else:
+        return ("No hay producto", 404)
